aca968450c8258b37858cb74480334e4467b4f65
Tivya1572 <71957463+Tivya1572@users.noreply.github.com>
2021-05-05 22:02:11 +0530

    Update and rename alternativeSorting.c to AlternativeSortingofMax&Min.c

--- Arrays/AlternativeSortingofMax&Min.c
+++ Arrays/AlternativeSortingofMax&Min.c
@@ -1,3 +1,13 @@
+// Alternate sorting: Given an array of integers, rearrange the array in such a way that the first element is first maximum and second element is first minimum and repeat the same sorting for the subsequent pairs
+
+// first element - first maximum
+// second element - first minimum
+// third element - second maximum
+// fourth element - second minimum ....
+
+// Example:
+// Input : {5, 7, 3, 1, 4, 6, 2}
+// Output : {7, 1, 6, 2, 5, 3, 4}
 #include<math.h>
 void maxmin(int a[],int size)
 {
